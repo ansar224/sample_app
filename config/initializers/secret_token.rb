931c228145bcf5ffe815611e8ffeed20c9246bce
U-Shani-PC\Shani <Shani@Shani-PC.(none)>
2013-08-04 15:06:29 +0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubymineProjects::Application.config.secret_token = 'c3752f4bfe0b3db44b592314488081b06138aa7befd1839ace14fe10205911e565d5bb5304664dcc2fa5c94e477b4fa646b8611c4165a4456e92ace5ef0dd6ff'
